76d11af1697e392eef467abdb1da12576d989fd8
aryanmonger20 <56786156+aryanmonger20@users.noreply.github.com>
2020-12-06 23:57:11 +0530

    header

--- frontend/auth/src/components/layouts/Header.js
+++ frontend/auth/src/components/layouts/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 
 export default function Header() {
     return (
         <div>
-            Header
+           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link Link to ="/">Home</Nav.Link>
+      <Nav.Link Link to ="/login">Login</Nav.Link>
+      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
         </div>
     )
 }
