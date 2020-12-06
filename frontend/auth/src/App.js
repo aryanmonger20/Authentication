8031ff509da66b74dc2b8c8487c7e294c3dab710
aryanmonger20 <56786156+aryanmonger20@users.noreply.github.com>
2020-12-06 23:39:01 +0530

    routing

--- frontend/auth/src/App.js
+++ frontend/auth/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import Home from './components/pages/Home'
+import Login from './components/auth/Login'
+import Register from "./components/auth/Register"
+import Header from "./components/layouts/Header"
+import "./style.css"
 
 export default function App() {
   return (
     <div>
+        <BrowserRouter>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path ="/login" component ={Login}/>
+            <Route path="/register" component={Register}/>
+          </Switch>
+          </BrowserRouter>
       
     </div>
   )
