bd72c949baa381878a583215f24a74de2c8025fb
Aryan Gupta <aryangupta007yo@gmail.com>
2021-01-14 23:11:08 +0530

    f1

--- frontend/auth/src/App.js
+++ frontend/auth/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-
-import {BrowserRouter,Switch,Route} from "react-router-dom";
-import Home from './components/pages/Home'
-import Login from './components/auth/Login'
-import Register from "./components/auth/Register"
 import Header from "./components/layouts/Header"
-
+import UserContext from "./context/UserContext"
 import "./style.css"
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Home from "./components/pages/Home"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+
 
 export default function App() {
+  //const [userData,setUserData]=useState
   return (
     <div>
         <BrowserRouter>
-        
-          <Header/>
-          <Switch>
+        <Header>
+        <Switch>
             <Route exact path="/" component={Home}/>
             <Route path ="/login" component ={Login}/>
             <Route path="/register" component={Register}/>
           </Switch>
-          </BrowserRouter>
-      
+        </Header>
+        </BrowserRouter>
     </div>
   )
 }
