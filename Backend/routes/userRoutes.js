51c043786c5b9b997e7f9cefe11ee55944b626dd
Aryan Gupta <aryangupta007yo@gmail.com>
2021-01-15 01:31:36 +0530

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -116,7 +116,8 @@ router.post("/tokenIsValid",async(req,res)=>{
 })
 router.get("/",auth,async (req,res)=>{
     const user =await User.findById(req.user)
-    res.json(user);
+    res.json({displayName :user.displayName,
+        id:user._id});
 })
 
 module.exports = router;
